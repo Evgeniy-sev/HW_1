563f172ad9c3b9907bb84936341529523884cb9a
Evgeniy-sev <121318784+Evgeniy-sev@users.noreply.github.com>
2022-12-25 21:55:18 +0300

    Add files via upload

--- creat_base.py
+++ creat_base.py
@@ -1,5 +1,5 @@
 # в качестве примера используется orm - SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, func,desc, extract, ForeignKeyConstraint, create_engine
+from sqlalchemy import Column, Integer, String, Float, DateTime, event, ForeignKey, func,desc, extract, ForeignKeyConstraint, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime, timedelta
@@ -11,6 +11,12 @@ engine = create_engine('sqlite:///test_sales.db', echo=True) # пример с s
 # declarative_base - фабричная функция, возвращающая базовый класс, от которого произойдет наследование класса с моделью.
 Base = declarative_base()
 
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 # Класс, описывающий одну из таблиц БД. Такой класс назывется моделью.
 
@@ -48,12 +54,13 @@ class Sales(Base):        #   продажи
     # Атрибуты класса описывают колонки таблицы, их типы данных и ограничения
     id = Column(Integer, primary_key=True, autoincrement=True, comment="Идентификатор операции")
     sale_time = Column(DateTime, nullable=False, default=datetime.now(), comment="время продажи")
-    item_id = Column(Integer, ForeignKey(Item.id), nullable=False, comment="Идентификатор товара")
-    store_id = Column(Integer, ForeignKey(Store.id), nullable=False, comment="Идентификатор магазина")
+    item_id = Column(Integer, nullable=False, comment="Идентификатор товара")
+    store_id = Column(Integer, nullable=False, comment="Идентификатор магазина")
 
-
-  #  __table_args__ = (
-  #      ForeignKeyConstraint(['item_id'], ['item.id']))
+    __table_args__ = (
+        ForeignKeyConstraint(['item_id'], ["item.id"]),
+        ForeignKeyConstraint(['store_id'], ["store.id"]),
+    )
 
     def __repr__(self):
         return "[id={0}, sale_time={1}, item_id={2}, store_id={3}]".format(
@@ -62,6 +69,7 @@ class Sales(Base):        #   продажи
             self.item_id,
             self.store_id)
 
+
 # создание таблиц если они не существуют
 Base.metadata.create_all(engine)
 
@@ -69,29 +77,30 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-session.query(Store).delete()              # заполнение Store
+session.query(Sales).delete()              # удаление Store
 session.commit()
 
-for i in range(1, 21):
+session.query(Item).delete()              # удаление Store
+session.commit()
+
+session.query(Store).delete()              # удаление Store
+session.commit()
+
+
+for i in range(1, 21):                  # заполнение Store
     new_store = Store(address = "г. Город, ул. Городская, д. " + str(i))
     session.add(new_store)
 session.commit()
 
 
-session.query(Item).delete()                # заполнение Item
-session.commit()
-
-for i in range(1, 31):
+for i in range(1, 31):                         # заполнение Item
     new_item = Item(name="Goods_" + str(i),
                     price=100 + i)
     session.add(new_item)
 session.commit()
 
 
-session.query(Sales).delete()                # заполнение Sales
-session.commit()
-a=timedelta(days=31)
+a = timedelta(days=31)                            # заполнение Sales
 
 for t in [datetime.now(), datetime.now()+a]:
     for i in range(10, 31):
@@ -125,7 +134,18 @@ for t in [datetime.now(), datetime.now()+a]:
                              sale_time=t)
             session.add(new_item)
 
-session.commit() 
+session.commit()
+
+
+new_item = Sales(item_id=1,
+                 store_id=100)
+try:
+    session.add(new_item)
+    session.commit()
+except Exception as E:
+    session.rollback()
+    print("****************************************************")
+    print(E)
 
 
 session.close()
